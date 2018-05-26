2d7ca7a00c3314b37e3b22d15e8675f10880cf1d
tinyking <akingchao@qq.com>
2018-05-26 23:26:03 +0800

    login css

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
-import { MatSidenavModule, MatIconModule, MatCardModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatSidenavModule, MatIconModule, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
@@ -13,6 +13,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatIconModule,
     MatCardModule,
     MatFormFieldModule,
+    MatButtonModule,
     MatInputModule,
     FlexLayoutModule,
     LoginRoutingModule
