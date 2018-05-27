6907d7c7b0a7a52be1aa5e9d5edf06787e90b0fa
王建朝 <wangjianchao1@jd.com>
2018-05-27 13:47:36 +0800

    update

--- src/app/pages/components/vertical-menu/vertical-menu.component.ts
+++ src/app/pages/components/vertical-menu/vertical-menu.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-vertical-menu',
-  templateUrl: './vertical-menu.component.html',
+  // templateUrl: './vertical-menu.component.html',
+  template: `<ng-content></ng-content>`,
   styleUrls: ['./vertical-menu.component.scss']
 })
 export class VerticalMenuComponent implements OnInit {
