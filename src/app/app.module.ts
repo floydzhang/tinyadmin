2a0ceb2af16c8046ec345ceeed9ae28bc4056052
tinyking <akingchao@qq.com>
2018-05-28 21:14:26 +0800

    update file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule, MatButtonModule, MatIconModule, MatCardModule, MatFormFieldModule, MatInputModule } from '@angular/material';
-
-
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { SelectivePreloadingStrategy } from './selective-preloading-strategy';
-import { MainRoutingModule } from './main/main-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+
+// custom module
+import { AppRoutingModule } from './app-routing.module';
+
+// component
+import { AppComponent } from './app.component';
+import { MainRoutingModule } from './main/main-routing.module';
 import { NotfoundComponent } from './notfound/notfound.component';
 
+import { SelectivePreloadingStrategy } from './selective-preloading-strategy';
 
 @NgModule({
   declarations: [
